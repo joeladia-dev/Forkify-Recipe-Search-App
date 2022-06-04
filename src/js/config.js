5fcb2dbb4bd517d0e4c0864cf2f46c16ce2c9d0a
joeladia-dev <adiajoel18@gmail.com>
2022-06-04 23:06:20 +0800

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cb3375bd-cde0-4879-9044-b6109f5ed5dd';
+export const MODAL_CLOSE_SEC = 2.5;
